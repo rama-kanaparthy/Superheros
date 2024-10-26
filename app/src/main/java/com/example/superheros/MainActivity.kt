ed617e66262651a3c5f6d9434a00d97a26832c5d
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2024-10-27 02:02:52 +0530

    compose ui

--- app/src/main/java/com/example/superheros/MainActivity.kt
+++ app/src/main/java/com/example/superheros/MainActivity.kt
@@ -1,11 +1,39 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.superheros
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superheros.data.HeroesRepository.heroes
+import com.example.superheros.model.Hero
 import com.example.superheros.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,7 +42,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperheroesTheme {
-                SuperherosApp()
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    SuperherosApp()
+                }
             }
         }
     }
@@ -22,7 +52,66 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SuperherosApp() {
+    Scaffold (
+        topBar = {
+            SuperHeroesAppbar()
+        }
+    ){ it ->
+        LazyColumn(contentPadding = it) {
+            items(heroes) {
+                HeroItem(
+                    hero = it,
+                    modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
+        }
+    }
+}
 
+@Composable
+fun HeroItem(hero: Hero, modifier: Modifier =Modifier){
+
+    Card(modifier = modifier) {
+        Row (modifier =  Modifier.fillMaxWidth()){
+            Column(modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(R.dimen.padding_medium),
+                end = dimensionResource(R.dimen.padding_medium)
+            )) {
+                Text(
+                    text = stringResource(hero.nameRes),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Text(
+                    text = stringResource(hero.descriptionRes),
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+            Spacer(Modifier.weight(1f))
+            Image(
+                painter = painterResource(hero.imageRes),
+                contentDescription = stringResource(hero.descriptionRes),
+                modifier = modifier
+                    .size(dimensionResource(R.dimen.image_size))
+                    .padding(dimensionResource(R.dimen.padding_small))
+                    .clip(MaterialTheme.shapes.small),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+@Composable
+fun SuperHeroesAppbar(){
+    CenterAlignedTopAppBar(
+        title = {
+            Row (verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
