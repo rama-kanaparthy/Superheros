0e89bbb5668567781c5a1ed2f99a2cc1d5afc717
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2024-10-27 01:29:58 +0530

    comp[ose changes

--- app/src/main/java/com/example/superheros/MainActivity.kt
+++ app/src/main/java/com/example/superheros/MainActivity.kt
@@ -4,21 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.superheros.ui.theme.SuperherosTheme
+import com.example.superheros.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SuperherosTheme {
+            SuperheroesTheme {
                 SuperherosApp()
             }
         }
@@ -33,7 +28,7 @@ fun SuperherosApp() {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SuperherosTheme {
+    SuperheroesTheme {
         SuperherosApp()
     }
 }
