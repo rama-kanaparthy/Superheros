25460d5e6c95b37be9fedd692a589c413d63ee1e
Rama Brahamam Kanparthy <ramabrahamam1991@gmail.com>
2024-10-27 00:44:24 +0530

    Added theme, color and Shapes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,8 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material3:material3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
